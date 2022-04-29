4a63caec55063d3b9c10dc7f72cad52b3faafb7f
Abhishek <abhishekphd14@gmail.com>
2022-04-29 13:57:44 +0530

    Update main.dart

--- Flutter/flutter_application_1/lib/main.dart
+++ Flutter/flutter_application_1/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/home_page.dart';
+import 'package:flutter_application_1/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage()
-      
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      // ),
+      // initialRoute: '/home',
+
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => const LoginPage()
+      },
     );
   }
 }
